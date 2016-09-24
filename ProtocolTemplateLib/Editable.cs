829a444627d282cec8c148b5494968383dcbc394
Lebedev Aleksey <leb-al@outlook.com>
2016-09-24 18:41:51 +0300

    Добавил Nlog (неотконфигурирова его) и поменял версии фреймворка проекта

---
+++ ProtocolTemplateLib/Editable.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProtocolTemplateLib
+{
+    public abstract class Editable
+    {
+        public bool EnableOtherField;
+    }
+
+    public class ComboboxEditable
+    {
+        public String[] Variants { get; set; }
+    }
+}
