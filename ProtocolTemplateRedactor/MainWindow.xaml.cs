3577a781e9a3866d0cc45722289ac67bde4397e6
kagudkov <kagudkov@gmail.com>
2016-10-01 22:44:09 +0300

    Добавил GUI, есть довольно много вопросов=)

--- ProtocolTemplateRedactor/MainWindow.xaml.cs
+++ ProtocolTemplateRedactor/MainWindow.xaml.cs
@@ -23,5 +23,10 @@ namespace ProtocolTemplateRedactor
         {
             InitializeComponent();
         }
+
+        private void textBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
     }
 }
