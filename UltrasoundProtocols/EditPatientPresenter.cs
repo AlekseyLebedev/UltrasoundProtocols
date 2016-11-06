f4398d01a6fa57c90d3374179b565cf5ec5c2779
kagudkov <kagudkov@gmail.com>
2016-11-06 14:18:23 +0300

    Добавил выгрузку контактов из базы данных

--- UltrasoundProtocols/EditPatientPresenter.cs
+++ UltrasoundProtocols/EditPatientPresenter.cs
@@ -16,9 +16,9 @@ namespace UltrasoundProtocols
         {
             //TODO загрузить из базы данных лист с данными о пациентах
             List<Patient> patientList = new List<Patient>();
-            patientList = selector.getPatients();
             patientList.Add(new Patient(0, "Александр", "Сергеевич", "Пушкин", PatientGender.Man, new DateTime(1799, 6, 6), "0"));
             patientList.Add(new Patient(1, "Петр", "Алексеевич", "Романов", PatientGender.Man, new DateTime(1672, 6, 9), "123"));
+            patientList.AddRange(selector.getPatients());
             return patientList;
         }
 
