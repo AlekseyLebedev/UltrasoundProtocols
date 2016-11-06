6049968db5d3c609d809b48040e99ff40956ba6e
valeriyknyazhev <valeriy.knyazhev@yandex.ru>
2016-11-06 12:53:26 +0300

    созданы классы DataBaseConnector и DataBaseSettings

---
+++ ProtocolTemplateLib/DataBaseConnector.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SqlClient;
+
+namespace ProtocolTemplateLib
+{
+	class DataBaseConnector
+	{
+		private SqlConnection Connection { get; set; }
+
+		public SqlCommand Command { get; set; }
+
+		private DataBaseSettings dataBaseSettings;
+
+		public DataBaseConnector(DataBaseSettings settings)
+		{
+			dataBaseSettings = settings;
+		}
+
+		public void CreateConnection()
+		{
+			try
+			{
+				Connection = new SqlConnection(dataBaseSettings.GetConnectionString());
+				Command = Connection.CreateCommand();
+				Connection.Open();
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine(e.Message + "Connection failed.");
+			}
+		}
+
+		public void CloseConnection()
+		{
+			try
+			{
+				Connection.Close();
+				Connection.Dispose();
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine(e.Message + "Connection was not closed.");
+			}
+		}
+
+		//SqlCommand GetSqlCommand()
+		//{
+		//	return Command;
+		//}
+
+	}
+}
