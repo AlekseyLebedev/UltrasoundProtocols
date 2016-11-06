ba45e976f4d25fc45919e135601f0d2c07a3cf55
IvanShafran <vanobox07@mail.ru>
2016-11-06 12:03:10 +0300

    added DataBaseUser

---
+++ UltrasoundProtocols/DataBaseUser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UltrasoundProtocols
+{
+    class DataBaseUser
+    {
+        string login { public get; public set; }
+        string password { public get; public set; }
+    }
+}
