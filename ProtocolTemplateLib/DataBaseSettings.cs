997fb8efaa3ca793e3c1a4e159ff5a63648ac2f2
IvanShafran <vanobox07@mail.ru>
2016-11-06 13:23:29 +0300

    сделал поля у Settings публичными

--- ProtocolTemplateLib/DataBaseSettings.cs
+++ ProtocolTemplateLib/DataBaseSettings.cs
@@ -13,11 +13,11 @@ namespace ProtocolTemplateLib
 			return String.Format(connectionPropertiesString, DataSource, Login, Password);
 		}
 
-		private string Login { get; set; }
+		public string Login { get; set; }
 
-		private string Password { get; set; }
+        public string Password { get; set; }
 
-		private string DataSource { get; set; }//"VALERIYPC\\SQLEXPRESS"
+        public string DataSource { get; set; }//"VALERIYPC\\SQLEXPRESS"
 
 		private const string connectionPropertiesString =
 			"Data Source='{0}'; Integrated Security=SSPI; Initial Catalog=UltraSoundProtocolsDB; User='{1}'; Password='{2}'";
