e7aaec421a1e66af1ff8dfb49ccf228c5304f0ff
Lebedev Aleksey <leb-al@outlook.com>
2016-11-27 00:38:34 +0300

    Hotfix запросов к БД

--- SQLRequests/SQLCreateBasicTables.sql
+++ SQLRequests/SQLCreateBasicTables.sql
@@ -4,9 +4,9 @@ CREATE TABLE
 	Tbl_Doctors
     (
 		dct_id INT NOT NULL IDENTITY(1, 1) PRIMARY KEY,
-		dct_firstname VARCHAR(255) NOT NULL,
-		dct_middlename VARCHAR(255) NULL,
-		dct_lastname VARCHAR(255) NOT NULL,
+		dct_firstname NVARCHAR(255) NOT NULL,
+		dct_middlename NVARCHAR(255) NULL,
+		dct_lastname NVARCHAR(255) NOT NULL,
 		dct_status BIT NOT NULL DEFAULT 1
 	);
 
@@ -14,27 +14,27 @@ CREATE TABLE
 	Tbl_Patients
 	(
 		pat_id INT NOT NULL IDENTITY(1, 1) PRIMARY KEY,
-		pat_firstname VARCHAR(255) NOT NULL,
-		pat_middlename VARCHAR(255) NULL,
-		pat_lastname VARCHAR(255) NOT NULL,
+		pat_firstname NVARCHAR(255) NOT NULL,
+		pat_middlename NVARCHAR(255) NULL,
+		pat_lastname NVARCHAR(255) NOT NULL,
 		pat_gender INT NOT NULL,
 		pat_birthdate DATE NOT NULL,
-		pat_numberambulatorycard VARCHAR(255) NULL
+		pat_numberambulatorycard NVARCHAR(255) NULL
 	);
 
 CREATE TABLE
 	Tbl_MedicalEquipments
 	(
 		meq_id INT NOT NULL IDENTITY(1, 1) PRIMARY KEY,
-		meq_name VARCHAR(255) NOT NULL
+		meq_name NVARCHAR(255) NOT NULL
 	);
 
 CREATE TABLE
 	Tbl_Templates
 	(
-		tem_id VARCHAR(255) NOT NULL PRIMARY KEY,
-		tem_name VARCHAR(255) NOT NULL,
-		tem_template VARCHAR(max) NOT NULL
+		tem_id NVARCHAR(255) NOT NULL PRIMARY KEY,
+		tem_name NVARCHAR(255) NOT NULL,
+		tem_template NVARCHAR(max) NOT NULL
 	);
 
 CREATE TABLE
@@ -45,12 +45,12 @@ CREATE TABLE
 		prt_doctor INT NOT NULL,
 		prt_patient INT NOT NULL,
 		prt_equipment INT NOT NULL,
-		prt_source VARCHAR(255) NULL
+		prt_source NVARCHAR(255) NULL
 	);
 
 CREATE TABLE
 	Tbl_Source
 	(
 		src_id INT NOT NULL IDENTITY(1, 1) PRIMARY KEY,
-		src_value VARCHAR(255) NOT NULL
+		src_value NVARCHAR(255) NOT NULL
 	);
--- SQLRequests/SQLDropBasicTables.sql
+++ SQLRequests/SQLDropBasicTables.sql
@@ -1,7 +1,6 @@
 ﻿DROP TABLE Tbl_Doctors;
-DROP TABLE Tbl_Gender;
 DROP TABLE Tbl_Patients;
 DROP TABLE Tbl_MedicalEquipments;
-DROP TABLE Tbl_ExaminationTypes;
+DROP TABLE Tbl_Templates;
 DROP TABLE Tbl_Protocols;
 DROP TABLE Tbl_Source;
